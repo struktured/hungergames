e287cbd4e146bf2b4a0f007abc5370a3a5a0b09e
Carmelo Piccione <mandelbrot>
2013-07-22 21:19:46 -0400

    weighted randomized bot

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from __future__ import division, print_function
 from Game import Game
-from bots import Pushover, Freeloader, Alternator
+from bots import Pushover, Freeloader, Alternator, RandomPlayer
 
 # Bare minimum test game. See README.md for details.
 
 if __name__ == '__main__':
-    players = [Pushover(), Freeloader(), Alternator()]
+    players = [Pushover(), Freeloader(), Alternator(), RandomPlayer([1.0,.0])]
     game = Game(players)
     game.play_game()
--- bots.py
+++ bots.py
@@ -1,4 +1,5 @@
 from Player import BasePlayer
+from util.math import weighted_choice
 
 class Pushover(BasePlayer):
     '''Player that always hunts.'''
@@ -52,4 +53,20 @@ class Alternator(BasePlayer):
                     ):
         self.update_strat()
         return [self.moves[0]]*len(player_reputations)
+
+class RandomPlayer(BasePlayer): 
+    '''Player that alternates between hunting and not.'''    
+    def __init__(self, dist=[.5, .5]):        
+        self.name = "Random(" + str(dist) + ")" 
+        self.moves = ['s', 'h']
+        self.dist = dist
         
+    def hunt_choices(
+                    self,
+                    round_number,
+                    current_food,
+                    current_reputation,
+                    m,
+                    player_reputations,
+                    ):
+        return [self.moves[weighted_choice(self.dist)] for _ in range(len(player_reputations))]
